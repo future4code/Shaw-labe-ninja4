03d5d28e2af62bd76ba63fcafc117e62b3689a6c
Pablo Gomes <pablogomes_bm@hotmail.com>
2022-04-11 12:35:24 -0300

    esqueleto projeto

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { AppContainer } from './components/AppContainer'
+import styled from 'styled-components'
+import  axios from 'axios'
 
-function App() {
+
+export default class App extends React.Component {
+	render(){
 	return (
         <AppContainer />
 	)
 }
-
-export default App
+}
