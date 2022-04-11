6b83a8946f1c6370a8ba0c422f071a4151fa6214
Rodrigo Vieira Batista dos Santos <rodrigo.v.santos89@gmail.com>
2022-04-11 17:22:24 -0300

    esqueleto do projeto com troca de telas e home page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,60 @@
-import React from 'react'
-import { AppContainer } from './components/AppContainer'
-import styled from 'styled-components'
-import  axios from 'axios'
+import React from "react";
+import { AppContainer } from "./components/AppContainer";
+import styled from "styled-components";
+import axios from "axios";
+import Home from "./pages/Home";
+import CadastroServicos from "./pages/CadastroServicos";
+import PageCarrinho from "./pages/PageCarrinho";
+import PageMostrarServicos from "./pages/PageMostrarServicos";
+import Footer from "./components/Footer";
+import ComponenteHeader from "./components/ComponenteHeader";
 
+const headers = {
+  headers: {
+    Authorization: "34cb6ce8-5c1e-4c13-8f08-adc127e1cd24",
+  },
+};
 
 export default class App extends React.Component {
-	render(){
-	return (
-        <AppContainer />
-	)
-}
+  state = {
+    telaAtual: "home",
+  };
+
+  escolherTela = () => {
+    switch (this.state.telaAtual) {
+      case "home":
+        return <Home />;
+      case "cadastro":
+        return <CadastroServicos />;
+      case "carrinho":
+        return <PageCarrinho />;
+      case "servicos":
+        return <PageMostrarServicos />;
+      default:
+        return <Home />;
+    }
+  };
+
+  mudaTela = (nomeTela) => {
+    this.setState({ telaAtual: nomeTela });
+  };
+
+  render() {
+    return (
+      <>
+        <div>
+          <button onClick={() => this.mudaTela("home")}>Home</button>
+		  <button onClick={() => this.mudaTela("carrinho")}>
+            Carrinho
+          </button>
+		  <button onClick={() => this.mudaTela("cadastro")}>
+            Quero ser um Ninja
+          </button>
+         
+          <button onClick={() => this.mudaTela("servicos")}>Contratar um Ninja</button>
+          <>{this.escolherTela()}</>
+        </div>
+      </>
+    );
+  }
 }
---
+++ src/pages/PageMostrarServicos.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import axios from "axios";
+import styled from "styled-components";
+
+const headers = {
+  headers: {
+    Authorization: "34cb6ce8-5c1e-4c13-8f08-adc127e1cd24",
+  },
+};
+
+export default class PageMostrarServicos extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>LabeNinjas</h1>
+        <h2>O talento certo no momento certo</h2>
+        <>SOU A PÁGINA DE CONTRATAR SERVIÇOS</>
+      </>
+    );
+  }
+}
